1b3e24c247dc9f5e3e07bd6656e86009d5865532
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-06 23:38:29 -0600

    Init to prepare list of products

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { products } from '../components/products'
+import ProductCart from '../components/productCart'
+
 
 const Home = () => {
   return (
     <>
       <h1 className='text-3xl my-5'>List Products</h1>
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 gap-5'>
+        {products.map((product,key) =>
+          <ProductCart key={key} dato={product}/>
+        )}
+      </div>
     </>
   )
 }
