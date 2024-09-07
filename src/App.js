5f1513fcef8195b6ba5e9f6dbb89fa28109dadf6
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-06 22:10:49 -0600

    Create components and pages.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-
+import Layout from './components/layout';
+import Home from './pages/home';
+import Detail from './pages/detail';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <>
