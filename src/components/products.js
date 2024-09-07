95e0d9de26e5ec0c35ff9fbc9710e925d4199488
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-06 23:31:02 -0600

    Add products to a file with js

--- src/components/products.js
+++ src/components/products.js
@@ -1,11 +1,75 @@
 import imageProduct1 from '../assets/images/1.jpeg'
+import imageProduct2 from '../assets/images/2.jpg'
+import imageProduct3 from '../assets/images/3.jpg'
+import imageProduct4 from '../assets/images/4.png'
+import imageProduct5 from '../assets/images/5.jpg'
+import imageProduct6 from '../assets/images/6.jpg'
+import imageProduct7 from '../assets/images/7.jpg'
+import imageProduct8 from '../assets/images/8.jpg'
+
 export const products = [
     {
         id: 1,
-        name: 'White tenis Jin Ping Fashion Soes',
+        name: 'White tenis Jin Ping Fashion Shoes',
         price: 250,
         image: imageProduct1,
         description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
         slug: "jin-ping-fashion"
+    },
+    {
+        id: 2,
+        name: 'Fashion Collection Best',
+        price: 350,
+        image: imageProduct2,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "fashion-collection-best"
+    },
+    {
+        id: 3,
+        name: 'Spureme',
+        price: 520,
+        image: imageProduct3,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "spureme"
+    },
+    {
+        id: 4,
+        name: 'Whish Shoes',
+        price: 280,
+        image: imageProduct4,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "whish-shoes"
+    },
+    {
+        id: 5,
+        name: 'Black Mirror Shoes',
+        price: 510,
+        image: imageProduct5,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "black-mirror-shoes"
+    },
+    {
+        id: 6,
+        name: 'Animal Print Tenis',
+        price: 370,
+        image: imageProduct6,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "animal-print-tenis"
+    },
+    {
+        id: 7,
+        name: 'Fashion Blue Shoes',
+        price: 280,
+        image: imageProduct7,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "fashion-blue-shoes"
+    },
+    {
+        id: 8,
+        name: 'Color Tenis Fashion',
+        price: 320,
+        image: imageProduct8,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "color-tenis-fashion"
     }
 ];
