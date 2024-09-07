10d7e14bcd0063f0a87b3bce49196b7be3cf071b
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-06 23:10:36 -0600

    start to add produts in a jss file

--- src/components/products.js
+++ src/components/products.js
@@ -1,2 +1,11 @@
+import imageProduct1 from '../assets/images/1.jpeg'
 export const products = [
-    {}];
+    {
+        id: 1,
+        name: 'White tenis Jin Ping Fashion Soes',
+        price: 250,
+        image: imageProduct1,
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing, elit cursus ligula massa mauris. Commodo iaculis est placerat viverra aenean, diam luctus bibendum tempus conubia, sodales gravida eget egestas.",
+        slug: "jin-ping-fashion"
+    }
+];
