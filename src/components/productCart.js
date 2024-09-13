aed9eada1a4aa58cb750282968bd4b1bea3c8065
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-13 16:44:37 -0600

    Añade una pestaña individual para cada producto.

--- src/components/productCart.js
+++ src/components/productCart.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const ProductCart = () => {
+const ProductCart = (props) => {
+  const {id, name, price, image, slug} = props.data;
+
   return (
-    <div>ProductCart</div>
+    <div className='bg-white, p-5 rounded-x1 shadow-sm'>
+      <Link to = {slug}>
+        <img src = {image} className='w-full h-80 object-cover object-top drop--[0_80px_30px_#0007]'/>
+      </Link>
+      <h3 className='text-2xl py-3 text-center font-medium'>{name}</h3>
+      <div className='flex justify-between items-center'>
+        <p>
+          $<span className='text-2xl font-medium'>{price}</span>
+        </p>
+        <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2'>
+          <img src='../assets/images/iconCart.png' className='w-5'/>
+          Add to Cart
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <h1 className='text-3xl my-5'>List Products</h1>
       <div className='grid lg:grid-cols-4 md:grid-cols-3 gap-5'>
         {products.map((product,key) =>
-          <ProductCart key={key} dato={product}/>
+          <ProductCart key={key} data={product}/>
         )}
       </div>
     </>
