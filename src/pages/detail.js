6497fabb3cc66ab37dc4d650304f90469a5bde70
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-15 12:06:38 -0600

    find details for the cart detail

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { products } from '../components/products';
 
 const Detail = () => {
+  const {slug} = useParams();
+  const {detail, setDeatil} = useState([]);
+
+  useEffect(() => {
+    const findDetail = products.filter(product => product.slud === slug);
+    if(findDetail.length>0){
+      setDeatil(findDetail[0]);
+    }else{
+      window.location.href = '/';
+    }
+  }, [slug])
+
   return (
     <div>Detail</div>
   )
