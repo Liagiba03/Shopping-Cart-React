4b1b081b8cb941010a302da11ac8ec605374de0e
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-18 23:02:40 -0600

    Add view of product details

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -20,7 +20,23 @@ const Detail = () => {
       <h2 className='text-3xl text-center'>PRODUCT DETAIL</h2>
       <div className='grid grid-cols-2 gap-5 mt-5'>
         <div>
-          <img src={detail.image} className=''/>
+          <img src={detail.image} className='w-full'/>
+        </div>
+        <div className='flex flex-col gap-5'>
+          <h1 className='text-4xl uppercase font-bold'>{detail.name}</h1>
+          <p className='font-bold text-3xl'>
+            ${detail.price}
+          </p>
+          <div className='flex gap-5'>
+            <div className='flex gap-2 justify-center items-center'>
+              <button className='bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center item'>+</button>
+              <span className='bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center item'>1</span>
+              <button className='bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center item'>-</button>
+            </div>
+            <button className='bg-slate-900 text-white px-7 py-3 rounded-xl shadow-2xl'>
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
