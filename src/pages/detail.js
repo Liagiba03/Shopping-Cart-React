a7696ea0959ef88a8394b9e9e19f262a0dc7080e
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-18 22:44:34 -0600

    Save the change on App.js. Show correctly the details

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -4,19 +4,26 @@ import { products } from '../components/products';
 
 const Detail = () => {
   const {slug} = useParams();
-  const {detail, setDeatil} = useState([]);
+  const [detail, setDetail] = useState([]);
 
   useEffect(() => {
-    const findDetail = products.filter(product => product.slud === slug);
-    if(findDetail.length>0){
-      setDeatil(findDetail[0]);
+    const findDetail = products.filter(product => product.slug === slug);
+    if(findDetail.length>0){  
+      setDetail(findDetail[0]);
     }else{
       window.location.href = '/';
     }
   }, [slug])
 
   return (
-    <div>Detail</div>
+    <div>
+      <h2 className='text-3xl text-center'>PRODUCT DETAIL</h2>
+      <div className='grid grid-cols-2 gap-5 mt-5'>
+        <div>
+          <img src={detail.image} className=''/>
+        </div>
+      </div>
+    </div>
   )
 }
 
