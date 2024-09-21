ed2d37ec2cd06b306927441f83c1311a5433b449
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-20 22:51:26 -0600

    Plus and minus quantity in the cart

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -11,6 +11,22 @@ const CartItem = (props) => {
     const findDetail = products.filter(product => product.id === productId)[0];
     setDetail(findDetail);
   }, [productId])
+
+  //FUncion para reducir la cantidad
+  const handleMinusQuantity = () =>{
+    dispatch(changeQuantity({
+      productId: productId,
+      quantity: quantity - 1
+    }))
+  }
+
+  //FUnction for reduce the quantity
+  const handlePlusQuantity = () =>{
+    dispatch(changeQuantity({
+      productId: productId,
+      quantity: quantity + 1
+    }))
+  }
   //console.log(detail)
   return (
     <div className='flex justify-between items-center bg-slate-600 text-white p-2 border-b-2 border-slate-700 gap-5 rounded-md'>
@@ -18,9 +34,9 @@ const CartItem = (props) => {
       <h3>{detail.name}</h3>
       <p>${detail.price * quantity}</p>
       <div className='flex justify-between gap-2'>
-        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600'>-</button>
+        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handleMinusQuantity}>-</button>
         <span>{quantity}</span>
-        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600'>+</button>
+        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handlePlusQuantity}>+</button>
       </div>
     </div>
   )
