73c4357da33356571551120c84d6a0395269c99a
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-20 20:43:58 -0600

    Add cart tab initial

--- src/components/cartTab.js
+++ src/components/cartTab.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CartTab = () => {
+  const carts = useSelector(store => store.cart.items);
   return (
-    <div>CartTab</div>
+    <div className='fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px]'>
+      CartTab
+    </div>
   )
 }
 
