0d6e2e53968dce0e738c2df918821b49ca3e12c1
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-09-20 22:06:50 -0600

    Add buttons for quantity and price

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -13,6 +13,12 @@ const CartItem = (props) => {
     <div className='flex justify-between items-center bg-slate-600 text-white p-2 border-b-2 border-slate-700 gap-5 rounded-md'>
       <img src={detail.image} className='w-12'></img>
       <h3>{detail.name}</h3>
+      <p>${detail.price * quantity}</p>
+      <div className='flex justify-between gap-2'>
+        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600'>-</button>
+        <span>{quantity}</span>
+        <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600'>+</button>
+      </div>
     </div>
   )
 }
